76691cf04fdb60746997a98b26553ef785f92d2a
miyamibu <225566405+miyamibu@users.noreply.github.com>
2026-07-08 22:18:13 +0900

    Prepare 1.0.14 store submissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ android {
         applicationId = "jp.miyamibu.urlalbum"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
